794a7e0d9d2bb3ce990b0cd896d6eaae39a2ae91
Ilia Konnov <iliakonnov@gmail.com>
2017-03-26 19:22:16 +0300

    Сделал сообщение об ошибках

--- main.js
+++ main.js
@@ -4,53 +4,66 @@ var endElem;
 var loadElem;
 var authElem;
 var mainElem;
+var manualLoadElem;
+var manualLoadBtnElem;
+var errorOccured = false;
 var allImagesShown = false;
 var authorized = false;
 var offset = 0;
 
 function urlify(text) {
-    return text.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1">$1</a>')
+    return text.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1">$1</a>');
 }
 
 function loadImages() {
     loadElem.show(0);
+    if (errorOccured) {
+        errorOccured = false;
+        manualLoadElem.hide(0);
+    }
     VK.Api.call('wall.get', {
         domain: 'pictures.yandex',
         count: 100,
         offset: offset
     }, function(r) {
-        if (r.response.length == 0) {
-            allImagesShown = true;
-            endElem.show(0);
-        }
-        r.response.forEach(function(element) {
-            if (element != null && typeof element == 'object' &&
-                "text" in element && "attachments" in element &&
-                element.text.indexOf("://vk.com/doc") != -1 &&
-                element.attachments.filter(el => el.type == "doc")
-            ) {
-                var date = new Date(element.date * 1000).toLocaleDateString();
-                var src = element.attachments.filter(el => el.type == "photo")[0].photo.src;
-                var bigSrc = element.attachments.filter(el => el.type == "photo")[0].photo.src_big;
-                var text = urlify(element.text);
-                var elem = $(
-                    '<div class="panel panel-default imagePanel">' +
-                    '    <div class="panel-heading">' + date + '</div>' +
-                    '    <div class="panel-body"><div class="thumbnail">' +
-                    '        <img src="' + src + '"></img>' +
-                    '        <div class="caption imageCaption"><p>' + text + '</p></div>' +
-                    '    </div></div>' +
-                    '</div>'
-                );
-                elem.find('img').mouseenter(function() {
-                    $(this).attr('src', bigSrc)
-                }).mouseleave(function() {
-                    $(this).attr('src', src)
-                });
-                elem.appendTo(mainElem);
+        if ("error" in r) {
+            alert('Error №' + r.error.error_code + ': ' + r.error.error_msg);
+            errorOccured = true;
+            manualLoadElem.show(0);
+        } else {
+            if (r.response.length == 0) {
+                allImagesShown = true;
+                endElem.show(0);
             }
-            offset++;
-        });
+            r.response.forEach(function(element) {
+                if (element != null && typeof element == 'object' &&
+                    "text" in element && "attachments" in element &&
+                    element.text.indexOf("://vk.com/doc") != -1 &&
+                    element.attachments.filter(el => el.type == "doc")
+                ) {
+                    var date = new Date(element.date * 1000).toLocaleDateString();
+                    var src = element.attachments.filter(el => el.type == "photo")[0].photo.src;
+                    var bigSrc = element.attachments.filter(el => el.type == "photo")[0].photo.src_big;
+                    var text = urlify(element.text);
+                    var elem = $(
+                        '<div class="panel panel-default imagePanel">' +
+                        '    <div class="panel-heading">' + date + '</div>' +
+                        '    <div class="panel-body"><div class="thumbnail">' +
+                        '        <img src="' + src + '"></img>' +
+                        '        <div class="caption imageCaption"><p>' + text + '</p></div>' +
+                        '    </div></div>' +
+                        '</div>'
+                    );
+                    elem.find('img').mouseenter(function() {
+                        $(this).attr('src', bigSrc)
+                    }).mouseleave(function() {
+                        $(this).attr('src', src)
+                    });
+                    elem.appendTo(mainElem);
+                }
+                offset++;
+            });
+        }
         loadElem.hide(0);
     });
 }
@@ -73,15 +86,18 @@ $(window).ready(function() {
     mainElem = $('#main');
     loadElem = $('#loader').hide(0);
     endElem = $('#end').hide(0);
+    manualLoadElem = $('#manualLoad').hide(0);
+    manualLoadBtnElem = $('#manualLoadBtn');
 
     VK.init({
         apiId: 5947241
     });
-    authElem.on('click', auth);
+    authElem.onclick(auth);
+    manualLoadBtnElem.onclick(loadImages);
 });
 
 $(window).scroll(function() {
-    if (!allImagesShown && authorized &&
+    if (!allImagesShown && authorized && !errorOccured &&
         $(window).scrollTop() + $(window).height() == $(document).height() // Bottom
     ) {
         loadImages();
