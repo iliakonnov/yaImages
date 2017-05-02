9d0ede24e2202abc058a3bb1048af6831d6a29a8
Ilia Konnov <iliakonnov@gmail.com>
2017-05-02 09:59:03 +0300

    Убрал загрузку через zip. Добавил предупреждение о загрузке

--- main.js
+++ main.js
@@ -205,10 +205,6 @@ function downloadAll() {
         // downloadModalText.text('Loading...');
         downloadModalText.text('Загрузка...');
 
-        // using jszip
-        var imgCount = 0;
-        var zipCount = 0;
-        var zip = new JSZip();
         $('.imagePanel').each(function(index, elem) {
             elem = $(elem);
             var url = elem.find('.downloadLink').prop('href');
@@ -218,92 +214,29 @@ function downloadAll() {
             if (url) {
                 $.ajax({
                     async: false,
-                    url: url, // my URL
+                    url: url,
                     type: "GET",
                     dataType: 'binary',
                     success: function(result) {
-                        zip.file(date + '.jpg', result);
+                        var link = document.createElement("a");
+                        link.download = name;
+                        link.hf = url;
+                        link.click();
+                        // sleep(2000);
                         var elem = $(
-                            '<a href="' + id +' target="_blank"">' + date + '</a><br/>: OK'
-                        ).appendTo(downloadModalBody);
-                        count++;
-                        if (count > 50)
-                        {
-                            count = 0;
-                            zip.generateAsync({type:"blob"})
-                            .then(function(content) {
-                                zipCount++;
-                                saveAs(content, "images"+zipCount+".zip");
-                            });
-                            var zip = new JSZip();
-                        }
+                            '<a href="' + id +' download="' + date + '.jpg" target="_blank"">' + date + '</a><br/>: OK'
+                        ).appendTo(downloadModalBody)[0].click();
                     },
                     error: function(result) {
-                        var elem = $(
-                            '<a href="' + id +' target="_blank"">' + date + '</a><br/>: Error'
-                        ).appendTo(downloadModalBody);
+                        var elem = $('<a href="' + id + '" target="_blank">' + date + '</a><br/>: Error');
+                        elem.appendTo(downloadModalBody);
                     }
                 });
             } else {
-                var elem = $(
-                    '<a href="' + id +' target="_blank"">' + date + '</a><br/>: Url not found'
-                ).appendTo(downloadModalBody);
+                var elem = $('<a href="' + id + '" target="_blank">' + date + '</a><br/>: Download link not found');
+                elem.appendTo(downloadModalBody);
             }
         });
-        /*
-        // Using zip.js
-        var tmpFilename = "tmp.zip";
-        requestFileSystem(TEMPORARY, 4 * 1024 * 1024 * 1024, function(filesystem) {
-            function create() {
-                filesystem.root.getFile(tmpFilename, {
-                    create : true
-                }, function(zipFile) {
-                    writer = new zip.FileWriter(zipFile);
-                    zip.createWriter(writer, function(writer) {
-                        zipWriter = writer;
-                        $('.imagePanel').each(function(index, elem) {
-                            elem = $(elem);
-                            var url = elem.find('.downloadLink').prop('href');
-                            var dateBtn = elem.find('.dateBtn')
-                            var date = dateBtn.text();
-                            var id = dateBtn.prop('href');
-                            if (url) {
-                                $.ajax({
-                                    async: false,
-                                    url: url, // my URL
-                                    type: "GET",
-                                    dataType: 'binary',
-                                    success: function(result) {
-                                        zipWriter.add(file.name, new zip.BlobReader(result), function() {}, function() {});
-                                        var elem = $(
-                                            '<a href="' + id +' target="_blank"">' + date + '</a><br/>: OK'
-                                        ).appendTo(downloadModalBody);
-                                    },
-                                    error: function(result) {
-                                        var elem = $(
-                                            '<a href="' + id +' target="_blank"">' + date + '</a><br/>: Error'
-                                        ).appendTo(downloadModalBody);
-                                    }
-                                });
-                            } else {
-                                var elem = $(
-                                    '<a href="' + id +' target="_blank"">' + date + '</a><br/>: Url not found'
-                                ).appendTo(downloadModalBody);
-                            }
-                        });
-                    }, console.log(message));
-                });
-            }
-            filesystem.root.getFile(tmpFilename, null, function(entry) {
-                entry.remove(create, create);
-            }, create);
-        });*/
-
-        var link = document.createElement("a");
-        link.download = 'images.zip';
-        link.href = zipFileEntry.toURL();;
-        link.click();
-
         $('<p>Done!</p>').appendTo(downloadModalBody);
         downloadModalCancel.prop('disabled', false);
         downloadModalStart.prop('disabled', false);
